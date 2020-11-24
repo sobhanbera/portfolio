70a21750700435e4f09ee44277fd587e4c467463
SobhanBera <sobhanbera258@gmail.com>
2020-11-24 16:07:45 +0530

    added about section

---
+++ src/components/about/AboutSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AboutSection.css";
+
+const About = () => {
+	return (
+		<div className='main-about'>
+			<div className='about-starts parallax-about'></div>
+		</div>
+	);
+};
+
+export default About;
