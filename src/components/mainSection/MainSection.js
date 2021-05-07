67ab3c9d972aa94bded510a1cddcdbbf85d887f8
SobhanBera <sobhanbera258@gmail.com>
2021-05-07 23:34:27 +0530

    added pdf temp and the link to download and see it.

--- src/components/mainSection/MainSection.js
+++ src/components/mainSection/MainSection.js
@@ -1,29 +1,39 @@
-import React from "react";
-import TextFlipper from "../elements/TextFlipper";
-import CodingCard from "./Cards/CodingCard";
-import "./MainSection.css";
+import React from 'react';
+import TextFlipper from '../elements/TextFlipper';
+import CodingCard from './Cards/CodingCard';
+import './MainSection.css';
 
-const MainSection = (props) => {
+const MainSection = props => {
 	const showResume = () => {
 		console.clear();
-		console.group("resume not available currently. Thanks for visiting.");
-		setTimeout(() => {
-			alert(
-				"Sorry currenlty resume is not available. As soon as I complete that I will upload it here."
-			);
-			console.group("Thanks for visiting. Explore The Website");
-		}, 500);
+		console.group('resume is now available. This is a temporary resume and will be removed soon. Thanks for visiting.');
+		// setTimeout(() => {
+		// 	alert('Sorry currenlty resume is not available. As soon as I complete that I will upload it here.');
+		// 	console.group('Thanks for visiting. Explore The Website');
+		// }, 500);
+
+		fetch('https://github.com/sobhanbera/portfolio/blob/master/src/assets/files/sobhanbera.pdf?raw')
+			.then(res => {
+				res
+					.blob()
+					.then(blob => {
+						let url = window.URL.createObjectURL(blob);
+						let a = document.createElement('a');
+						a.href = url;
+						a.download = 'sobhan-bera.pdf';
+						a.click();
+					})
+					.catch(err => {});
+			})
+			.catch(err => {});
 	};
+
 	return (
 		<div className={`main-home ${props.theme}`}>
 			<div className='main-home-section'>
 				<h2>Hi, Geeks. I am Sobhan Bera.</h2>
 				<TextFlipper theme={props.theme} />
-				<p>
-					Welcome to this website. This website is portfolio of mine
-					Here you can know about me in breif Scroll Up To know more
-					about me.
-				</p>
+				<p>Welcome to this website. This website is portfolio of mine Here you can know about me in breif Scroll Up To know more about me.</p>
 				<div>
 					<button onClick={showResume}>Resume</button>
 				</div>
